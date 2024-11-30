fe6bb0bd113ea22d177f893429c662987e24c242
jhoan <johanmonsalve125@gmail.com>
2024-11-30 08:26:50 -0500

    dashboard999999

--- src/app/(routes)/companies/[companiesId]/page.tsx
+++ src/app/(routes)/companies/[companiesId]/page.tsx
@@ -1,12 +1,16 @@
-import { db } from "@/lib/db"
-import { auth } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
-import { HeaderCompanyIdPage } from "./components/header"
-import { FooterComaniesIdPage } from "./components/footer"
-import { CompaniesInformation } from "./components/companyInformation"
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { HeaderCompanyIdPage } from "./components/header";
+import { FooterComaniesIdPage } from "./components/footer";
+import { CompaniesInformation } from "./components/companyInformation";
 
-export default async function CompaniesIdPage ({params}: {params: {companiesId: string}}) {
-    const { companiesId } = await params
+interface CompaniesIdPageProps {
+    params: { companiesId: string }; // Define explícitamente el tipo de params
+}
+
+export default async function CompaniesIdPage({ params }: CompaniesIdPageProps) {
+    const { companiesId } = params; // Extraemos `companiesId` de los parámetros de la ruta
     const { userId } = await auth();
 
     if (!userId) {
@@ -14,22 +18,21 @@ export default async function CompaniesIdPage ({params}: {params: {companiesId: 
     }
 
     const company = await db.company.findUnique({
-        where : {
-            id : companiesId,
-            userId
+        where: {
+            id: companiesId,
+            userId,
         },
-        
-    })
+    });
 
     if (!company) {
-        redirect("/")
+        redirect("/");
     }
 
     return (
         <div>
-            <HeaderCompanyIdPage/>
+            <HeaderCompanyIdPage />
             <CompaniesInformation company={company} />
-            <FooterComaniesIdPage company={company}/>
+            <FooterComaniesIdPage company={company} />
         </div>
-    )
+    );
 }
