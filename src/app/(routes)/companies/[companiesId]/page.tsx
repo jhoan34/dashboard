849ab54f3878d8be1c8ec3d6f724eecd17b66d1d
jhoan <johanmonsalve125@gmail.com>
2024-11-30 09:42:40 -0500

    dashboadqqqqqqqqqqqqqqqqqqqqqqqq

--- src/app/(routes)/companies/[companiesId]/page.tsx
+++ src/app/(routes)/companies/[companiesId]/page.tsx
@@ -5,10 +5,12 @@ import { HeaderCompanyIdPage } from "./components/header";
 import { FooterComaniesIdPage } from "./components/footer";
 import { CompaniesInformation } from "./components/companyInformation";
 
-export default async function CompaniesIdPage({ params }: { params: { companiesId: string } }) {
-    // Get the userId from Clerk's authentication system
-    const { userId } = await auth();
+type tParams = Promise<{ companiesId: string }>;
 
+export default async function CompaniesIdPage(props: { params: tParams }) {
+    const companiesId = (await props.params).companiesId;
+
+    const { userId } = await auth();
     // If no userId exists, redirect to login page
     if (!userId) {
         return redirect("/login");
@@ -17,7 +19,7 @@ export default async function CompaniesIdPage({ params }: { params: { companiesI
     // Fetch the company associated with the current user and companiesId
     const company = await db.company.findUnique({
         where: {
-            id: params.companiesId,  // Query by companiesId directly
+            id: companiesId,  // Query by companiesId directly
             userId,                   // Ensure the userId matches
         },
     });
